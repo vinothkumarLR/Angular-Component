29acfadbfc665dbdd378c396ba54c7bb5b57d46f
vinoth kumar <vinothrocksyou@gmail.com>
2016-10-26 00:10:54 +0530

    click event binded

--- app.js
+++ app.js
@@ -1,15 +1,26 @@
+// create module for the application
 var app = angular.module('myapp', []);
 
+// directive shows image tooltip
 app.directive('imgLoader',
 		function() {
 			return {
 				restrict : 'E',
 				template : '<a href="#" class="tooltip">Click here <span class="tooltip-holder"><span id="cross-icon"><img src="cross.png"></img></span><img class="callout" src="callout.gif"><img src="image.png"></span></a>',
-				controller : function($scope) {
-					console.log("inside controller");
-				},
-				link : function postLink(scope, iElement, iAttrs) {
-					console.log("inside post link");
+				link : function postLink(scope, elem, attr) {
+					
+					// to bind click event for component
+					elem.bind('click', function(e) {
+					   $('.tooltip-holder').css('display', 'block');
+					});
+					
+					// to hide image tooltip when cross icon clicked
+					$('#cross-icon').on('click', function (event) {
+						event.stopPropagation();
+						$('.tooltip-holder').css('display', 'none');
+					});
+					
 				}
 			};
 		});
+
